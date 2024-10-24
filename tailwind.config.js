bc4702eb33892589c90ab5c6d1a65e75e77d2db8
marsalan828 <marsalan828@gmail.com>
2024-10-24 19:12:46 +0500

    completed portfolio template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        navbarbg: '#be3144',
+        projectsbg :'#45567d',
+      },
+      backgroundImage:{
+        gradientcolor: 'linear-gradient(62deg, #3a3d40 0%, #181719 100%)',
+      },
+      fontFamily:{
+        raleway: ['Raleway', 'sans-serif'],
+      },
+      fontSize:{
+        heroHeading:'6rem',
+      },
+      colors: {
+        defaultcolor : '#be3144',
+        projectbg: '#303841',
+        coffecolor: '#8d9196',
+      }
+    },
   },
   plugins: [],
 }
